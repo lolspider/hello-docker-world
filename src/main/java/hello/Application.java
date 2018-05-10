c61a712001901843f693fe2e6b0b4e2e50a55b08
Chenglin Tian <cltian@CNcltian.local>
2018-05-10 19:54:04 +0800

    update to 0.2.0

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World";
+        return "Hello Docker World From Version 0.2.0";
     }
 
     public static void main(String[] args) {
